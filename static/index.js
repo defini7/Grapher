a77d7be8c0aeaf27bd3aecc5c5d61617400b0044
Alex <alexander.suhov@hotmail.com>
2022-03-29 16:07:48 +0300

    looks cool

--- static/index.js
+++ static/index.js
@@ -20,11 +20,11 @@ let offset = {
 }
 
 let alphas = {
-    'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
-    'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
-    'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
-    'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
-    'u': 0, 'v': 0, 'w': 0, 'x': 0, 'z': 0
+    'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1,
+    'f': 1, 'g': 1, 'h': 1, 'i': 1, 'j': 1,
+    'k': 1, 'l': 1, 'm': 1, 'n': 1, 'o': 1,
+    'p': 1, 'q': 1, 'r': 1, 's': 1, 't': 1,
+    'u': 1, 'v': 1, 'w': 1, 'x': 1, 'z': 1
 }
 
 function appendRow() {
@@ -254,7 +254,7 @@ function drawGraph(expression, color = 'red', width = 3) {
 
     const { x } = size
     const verticalSize = x / (50 * scale)
-    const points = 100 * verticalSize
+    const points = 200 * verticalSize
 
     context.beginPath()
     context.strokeStyle = color
@@ -288,7 +288,7 @@ function drawGraph(expression, color = 'red', width = 3) {
                     div.appendChild(slider)
                     document.querySelector('div[data-expressions]').appendChild(div)
 
-                    alphas[expression[i]] = 0
+                    alphas[expression[i]] = 1
                 }
             } else {
                 alphas[expression[i]] = s.value
